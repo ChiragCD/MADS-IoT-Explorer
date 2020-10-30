0dd00ee15d15dd6d47d4effbf9d3fc4549641567
arjun289 <arjun289singh@gmail.com>
2020-10-30 18:47:47 +0800

    Fix deps issue for poison

--- apps/acqdat_api/mix.exs
+++ apps/acqdat_api/mix.exs
@@ -59,9 +59,6 @@ defmodule AcqdatApi.MixProject do
       # sentry logging
       {:sentry, "~> 7.0"},
       {:hackney, "~> 1.8"},
-
-      # javascript execution
-      {:execjs, git: "https://github.com/devinus/execjs"}
     ]
   end
 end
