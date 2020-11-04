442601c41cf8cee178f94713c7d874c7d5fd218b
arjun289 <arjun289singh@gmail.com>
2020-11-04 15:28:32 +0800

    Add tests for script node

--- apps/acqdat_core/mix.exs
+++ apps/acqdat_core/mix.exs
@@ -106,9 +106,6 @@ defmodule AcqdatCore.MixProject do
       #stream processing and communications with kafka
       {:kafka_ex, "~> 0.11"},
 
-      # if using snappy compression
-      {:snappy, git: "https://github.com/fdmanana/snappy-erlang-nif"},
-
       # javascript execution
       {:execjs, git: "https://github.com/devinus/execjs"}
     ]
